1c24deb7f377122cf0128a0c46aed728013842fc
pro100v1ad <vlad36144@mail.ru>
2025-05-04 15:49:34 +0300

    Ничего не менял, тестовый коммит

--- src/main/java/com/magicode/Main.java
+++ src/main/java/com/magicode/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         GamePanel panel = new GamePanel();
 
+
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         f.setContentPane(panel);
 
